cc6450215eb567f0fe15006a4d0a590c3f6f8951
ErikStelarsen <Erikstelarsen@outlook.com>
2018-12-19 21:54:26 +0100

    gjort litt mer på et forslag til design

--- calisthenics-app/src/components/Header.js
+++ calisthenics-app/src/components/Header.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import NavBar from './NavBar';
 import '../styles/components/Header.css';
-
+import {NavLink} from 'react-router-dom';
 
 
 const Header = (props)  => (
     <div className="Header">
+        <NavLink to="/">
         <img className="img-header" src={props.picture} height="100" width="125"></img>
+        </NavLink>
         <NavBar />
     </div>
 )
