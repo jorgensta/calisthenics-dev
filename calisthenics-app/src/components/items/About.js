9a9d68052ffb20556556c47bdd6d33637935efbc
Jørgen Stamnes <jorghs@stud.ntnu.no>
2018-12-20 07:32:19 +0100

    Check if mail is changed

--- calisthenics-app/src/components/items/About.js
+++ calisthenics-app/src/components/items/About.js
@@ -5,6 +5,7 @@ import '../../styles/components/items/About.css';
 const About = () => (
     <div className="About">
         AccessToken: 3098531026.1677ed0.e8c4ee4585184111bc75a5506d2b0fbe
+        
     </div>
 )
 
