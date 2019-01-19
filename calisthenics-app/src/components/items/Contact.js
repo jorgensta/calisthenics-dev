f53d4ab8947125b7c9649d4d9dc39e1c871b97a8
Jørgen Stamnes <jorghs@stud.ntnu.no>
2019-01-19 12:35:03 +0100

    Started adding forms for mail contact

--- calisthenics-app/src/components/items/Contact.js
+++ calisthenics-app/src/components/items/Contact.js
@@ -2,17 +2,36 @@ import React from 'react';
 import '../../styles/components/items/Contact.css';
 
 
-const Contact = () => (
+class Contact extends React.Component {
+    
+    onSubmit(){
+
+    }
+
+
+    render(){
+        return(
     <div className="Contact">
         <div className="contactContainer">
             <div>
                 Facebook
             </div>
-            <div>
+            <div className="mailContainer">
+                <div>
                 Mail
+                </div>
+                <form onSubmit={this.onSubmit()}>
+                <div>
+                message
+                </div>
+                <textarea>
+                </textarea>
+                </form>
             </div>
         </div>
     </div>
-)
+    )}
+}
+
 
 export default Contact;
