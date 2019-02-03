15ea6e35cada40f7299dfa1359a1e12888346521
Jørgen Stamnes <jorghs@stud.ntnu.no>
2019-02-03 13:05:53 +0100

    Ordnet opp i stuff sånn at det funker

--- calisthenics-app/src/components/items/Home.js
+++ calisthenics-app/src/components/items/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../../styles/components/items/Home.css';
 import Zoom from 'react-reveal/Zoom';
-import picture from '../../assets/FrontpagePicture.jpg';
 
 
 const Home = () => {
@@ -9,7 +8,7 @@ const Home = () => {
         <div className="Home">
             <div className="homeTextDiv">
             <Zoom>
-            {/*<p className="frontText">Formed in 2015, NTNUI Calisthenics has grown
+            <p className="frontText">Formed in 2015, NTNUI Calisthenics has grown
             into a extraordinary group with enthusiasm, strength and companionship
             from a tremendous disparate group of students. We are currently
             Norways largest calisthenics community. The group is composed of
@@ -19,23 +18,18 @@ const Home = () => {
             <a href="https://docs.google.com/forms/d/18Juyj22opQFylISBQYKHBxOd4HtJR-gquAKkr5k35h4/" rel="noopener noreferrer" target="_blank" className="applyButton">Apply now!</a>
             </Zoom>
             </div>
-
-            {/* <Zoom>
-                <img className="frontPic" alt="Forsidebilde Calisthenics"src={picture}></img>
-            </Zoom>
-            */}
-
             <Zoom>
             <div className="scheduleDivContainer">
               <div className="scheduleDiv">
                 <div className="scheduleHeader">Training hours</div>
-                {/*<div className="scheduleTraining"><span className="boldFont">Tuesday</span> 18:30-19:55 Armfeldtsalen</div> */}
+                <div className="scheduleTraining"><span className="boldFont">Tuesday</span> 16:00-17:50 SiT Portalen</div>
                 <div className="scheduleTraining"><span className="boldFont">Friday</span> 19:00-21:00 Gymfit</div>
                 <div className="scheduleTraining"><span className="boldFont">Sunday</span> 19:00-21:00 Gymfit</div>
               </div>
             </div>
             </Zoom>
-
-        </div>
+            </div>
+            
     )}
+
 export default Home;
