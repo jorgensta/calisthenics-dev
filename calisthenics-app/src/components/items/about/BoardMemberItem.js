fe56b0030e7164c4fec696f63e710bfddeccf0be
Erikstelarsen <Erikstelarsen@outlook.com>
2019-02-03 17:38:35 +0100

    yo

--- calisthenics-app/src/components/items/about/BoardMemberItem.js
+++ calisthenics-app/src/components/items/about/BoardMemberItem.js
@@ -5,7 +5,7 @@ import React from 'react';
 const BoardMemberItem = ({member}) => (
     <div className="boardMember">
     <h1>{member.position}</h1>
-    <img width="200" height="200" src='https://scontent-dus1-1.xx.fbcdn.net/v/t1.0-9/13939524_10207032790442783_1146105930638870934_n.jpg?_nc_cat=105&_nc_ht=scontent-dus1-1.xx&oh=691e5a674c15fe031ac22b0ab5593c8a&oe=5CBA5B7D' ></img>
+    <img width="200" height="200" src='https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/13339535_1002433096477287_7306902061994640903_n.jpg?_nc_cat=111&_nc_ht=scontent-arn2-1.xx&oh=a7b8903fea249965f39f6d37579653c2&oe=5CF3BDC5' ></img>
     <h4>{member.firstName + ' ' + member.lastName}</h4>
     </div>
 )
