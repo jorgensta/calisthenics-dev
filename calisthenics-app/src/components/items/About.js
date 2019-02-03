3178ebe70ab80364ca2e7bc238ce5e5911d1081f
Jørgen Stamnes <jorghs@stud.ntnu.no>
2019-02-03 14:05:02 +0100

    React reveal effects on About us

--- calisthenics-app/src/components/items/About.js
+++ calisthenics-app/src/components/items/About.js
@@ -4,12 +4,16 @@ import BoardMemberItem from './about/BoardMemberItem';
 import '../../styles/components/items/About.css';
 import {boardMembers} from '../../assets/boardMembers/boardMembers';
 
+import Fade from 'react-reveal/Fade';
+
 
 const About = () => (
     <div className="About">
+    <Fade big>
     {boardMembers.map((member, index) => (
         <BoardMemberItem key={index} member={member} />
     ))}
+    </Fade>
     </div>
 )
 
