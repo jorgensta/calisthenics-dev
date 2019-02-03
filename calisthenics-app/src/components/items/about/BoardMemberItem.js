531d75329c3cbb2820ed8e87f9593da60ac8b74f
Jørgen Stamnes <jorghs@stud.ntnu.no>
2019-02-03 14:34:00 +0100

    Larsen is fine

--- calisthenics-app/src/components/items/about/BoardMemberItem.js
+++ calisthenics-app/src/components/items/about/BoardMemberItem.js
@@ -5,7 +5,7 @@ import React from 'react';
 const BoardMemberItem = ({member}) => (
     <div className="boardMember">
     <h1>{member.position}</h1>
-    <img width="200" height="150" src='https://scontent-dus1-1.xx.fbcdn.net/v/t1.0-9/303644_10150842567361685_1612897999_n.jpg?_nc_cat=106&_nc_ht=scontent-dus1-1.xx&oh=0217173bd2020581edbe8a31ca38ff73&oe=5CB7C245' ></img>
+    <img width="200" height="150" src='https://scontent-dus1-1.xx.fbcdn.net/v/t1.0-9/13939524_10207032790442783_1146105930638870934_n.jpg?_nc_cat=105&_nc_ht=scontent-dus1-1.xx&oh=691e5a674c15fe031ac22b0ab5593c8a&oe=5CBA5B7D' ></img>
     <h4>{member.firstName + ' ' + member.lastName}</h4>
     </div>
 )
